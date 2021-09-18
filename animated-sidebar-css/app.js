db9330d8b2684fa0a040d016f3f4107d5702f15b
andresacelas <andresacelas@outlook.com>
2021-09-18 16:31:59 -0500

    remove old components

--- animated-sidebar-css/app.js
+++
@@ -1,5 +0,0 @@
-const buttonToggle = document.querySelector(".toggle-btn");
-
-buttonToggle.addEventListener("click", function() {
-  document.getElementById("sidebar").classList.toggle("active");
-});
